e58fed1fb33e761b5391d42f028e7477b214a2dc
Clara <clarafdezfraga@gmail.com>
2024-05-05 12:11:30 +0200

    Add KillCharacter to character router

--- src/characters/routers/charactersRouter.ts
+++ src/characters/routers/charactersRouter.ts
@@ -7,4 +7,6 @@ charactersRouter.get("/", charactersControler.getCharacters);
 
 charactersRouter.get("/:id", charactersControler.getLocution);
 
+charactersRouter.patch("/:id", charactersControler.killCharacter);
+
 export default charactersRouter;
